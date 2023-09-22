8e456e3faf95189a4e2151fb82846ee414799816
mehraankush <mehraankush36@gmail.com>
2023-09-22 18:27:22 +0530

    added url of Db in env

--- db/db.js
+++ db/db.js
@@ -3,7 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // const URL = `mongodb+srv://${process.env.USER}:${process.env.Mongodb_Pass}@myrecipe.zzi4z2s.mongodb.net/myrecipe?retryWrites=true&w=majority`;
-const URL = `mongodb+srv://${process.env.USER}:${process.env.Mongodb_Pass}@procollab.mlrh89s.mongodb.net/procollab?retryWrites=true&w=majority`;
+// const URL = `mongodb+srv://${process.env.USER}:${process.env.Mongodb_Pass}@procollab.mlrh89s.mongodb.net/procollab?retryWrites=true&w=majority`;
+
+const URL = process.env.MONGO_URL;
+// console.log(URL);
 
 const connectDB  = async ()  =>{
     try{ 
