aed7b09f427b76787b73218aafeba8fc86f8cdbc
mehraankush <mehraankush36@gmail.com>
2023-09-22 18:17:40 +0530

    moved Db connection first

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 8000;
 var cors = require('cors')
 
 
+connectDB();
 app.use(cors())
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.json());
 app.use('/',require('./routes'));
 
-connectDB();
 app.listen(PORT, function(){
     console.log("Server is running on port ",PORT);
 });
